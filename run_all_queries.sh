577ed75ecf17f6dc817571e4fdc416fb7474df2f
eliisabetk <eliisabet.kaasik@gmail.com>
2026-05-22 09:17:54 +0200

    added validation script

--- run_all_queries.sh
+++ run_all_queries.sh
@@ -19,3 +19,17 @@ for i in {1..20}; do
     python3 ../../reducer.py --query original_test.sql --test test.sh
   )
 done
+
+echo ""
+echo "--- VALIDATION ---"
+for i in {1..20}; do
+  dir="queries/query$i"
+  tmp=$(mktemp)
+  tr -d '\r' < "$dir/test.sh" > "$tmp"
+  if TEST_CASE_LOCATION="$dir/query.sql" bash "$tmp" 2>/dev/null; then
+    echo "query$i: OK"
+  else
+    echo "query$i: FAIL"
+  fi
+  rm -f "$tmp"
+done
